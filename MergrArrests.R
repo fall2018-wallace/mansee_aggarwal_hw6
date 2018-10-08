42823d6330011739b03046169b8009f65b348d09
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 18:07:50 -0400

    Saving MergrArrests.R

--- MergrArrests.R
+++ MergrArrests.R
@@ -4,7 +4,7 @@ arrests<-USArrests
 df_final<-merge(x=census,y=arrests,by.x="stateName",by.y="row.names")
 str(df_final)
 
-library(ggplot)
+library(ggplot2)
 
 plot1<-ggplot(df_final,aes(x=population)) +
   geom_histogram(color="black",fill="red",binwidth=10)
