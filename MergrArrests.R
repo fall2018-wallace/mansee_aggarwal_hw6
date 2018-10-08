dd84819b2cddb4c9b55e3fd1010f80a98651681f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 18:05:30 -0400

    Saving MergrArrests.R

--- MergrArrests.R
+++ MergrArrests.R
@@ -1,7 +1,7 @@
 
 arrests<-USArrests
 
-df_final<-merge(x=dfStates,y=arrests,by.x="stateName",by.y="row.names")
+df_final<-merge(x=census,y=arrests,by.x="stateName",by.y="row.names")
 str(final_df)
 
 
