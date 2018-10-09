0d713a81744db3d7cc452b6478d2a34b5773e55c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 15:59:53 -0400

    Saving MergrArrests.R

--- MergrArrests.R
+++ MergrArrests.R
@@ -12,11 +12,13 @@ plot1<-ggplot(df_final,aes(x=population)) +
 plot2<-ggplot(df_final,aes(x=Murder)) +
   geom_histogram(color="black",fill="red",bin=20)
   
-plot3<-ggplot(df_final,aes(x=Murder)) +
+plot3<-ggplot(df_final,aes(x=Assault)) +
   geom_histogram(color="black",fill="red",bin=20)
-plot4<-ggplot(df_final,aes(x=Murder)) +
+  
+plot4<-ggplot(df_final,aes(x=UrbanPop)) +
   geom_histogram(color="black",fill="red",bin=20)
-plot5<-ggplot(df_final,aes(x=Murder)) +
+  
+plot5<-ggplot(df_final,aes(x=Rape)) +
   geom_histogram(color="black",fill="red",bin=20)
 
 
