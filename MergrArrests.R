70e703c1ce24e1d1cf6bc83abced0ed4c6245f49
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 16:09:51 -0400

    Saving MergrArrests.R

--- MergrArrests.R
+++ MergrArrests.R
@@ -21,5 +21,5 @@ plot4<-ggplot(df_final,aes(x=UrbanPop)) +
 plot5<-ggplot(df_final,aes(x=Rape)) +
   geom_histogram(color="black",fill="red",bin=20)
 
-plot6<-ggplot(df_final,aes(x=population)) +
+plot6<-ggplot(df_final,aes(y=population)) +
        geom_boxplot()
