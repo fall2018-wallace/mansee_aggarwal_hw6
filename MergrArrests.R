c17c207501313b6601c46db2c2e399bf2d9dd179
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 16:03:25 -0400

    Saving MergrArrests.R

--- MergrArrests.R
+++ MergrArrests.R
@@ -36,6 +36,6 @@ plot8<-ggplot(df_final,aes(x=stateName,y=murder_state)) +
        geom_col()
        
 plot9<-ggplot(df_final,aes(x=reorder(stateName),y=murder_state)) +
-       geom_col()
+       geom_col() + theme(axis.text.x=element_text(angle=90,hjust=1))
 
        
