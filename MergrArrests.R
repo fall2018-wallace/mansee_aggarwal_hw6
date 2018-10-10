179e2b4144ebf1ebbacd1bf85f7e3108b9ecd00d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 16:23:29 -0400

    Saving MergrArrests.R

--- MergrArrests.R
+++ MergrArrests.R
@@ -43,5 +43,9 @@ plot9<-ggplot(df_final,aes(x=stateName,murder_state,y=murder_state)) +
 plot10<-ggplot(df_final,aes(x=reorder(stateName,murder_state),y=murder_state)) +
        geom_col() + theme(axis.text.x=element_text(angle=90,hjust=1))+
        ggtitle("Total Murders")
+       
+plot11<-ggplot(df_final,aes(x=reorder(stateName,murder_state),y=murder_state,fill=percentOver18)) +
+       geom_col() + theme(axis.text.x=element_text(angle=90,hjust=1))+
+       ggtitle("Total Murders")
 
        
