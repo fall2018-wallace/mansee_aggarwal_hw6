1a86e8d7d96613b70487e9b8a6d17a0509db22c7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:36:49 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -7,11 +7,12 @@ states<-states[-1,]
 num.row<-nrow(states)
 #removing last row
 states<-states[-num.row,]
+#removing columns 1 to 4
 states<-states[,-1:-4]
-
+#changing column names
 colnames(states)<-c("stateName","population","popOver18","percentOver18")
 return(states)
 }
-
+#retured clean dataset 
 cleanCensus<-readStates(raw_data)
 str(cleanCensus)
